bb12b47dc978be91a8855717814ac0badf344ae0
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-07-15 17:59:59 +0530

    LandingController Phase - 1, with returning services available

--- server/controllers/landingController.js
+++ server/controllers/landingController.js
@@ -1,5 +1,13 @@
+const services = require("../models/services");
+
 const landingController = async (req, res) => {
-  res.status(200).send("Landing Page!");
+  try {
+    const allService = await services.find();
+    console.log(allService);
+    res.status(200).json({ allService });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 };
 
 module.exports = landingController;
