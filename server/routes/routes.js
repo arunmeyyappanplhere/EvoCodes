5272ee7710a82e0348f4139249aaaa5d8d531bd8
deepak-2007-dev <deepakkingtry@gmail.com>
2026-07-15 21:11:46 +0530

    projects page

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,12 +3,13 @@ const contactController = require("../controllers/contactController");
 const landingController = require("../controllers/landingController");
 const servicesController = require("../controllers/servicesController");
 const testAddServices = require("../controllers/testAddServices");
-
+const projectsController = require("../controllers/projectsController");
 const route = express.Router();
 
 route.post("/contact", contactController);
 route.get("/", landingController);
 route.get("/services", servicesController);
+route.get("/projects", projectsController);
 
 
 // route.post("/AddTest", testAddServices);
