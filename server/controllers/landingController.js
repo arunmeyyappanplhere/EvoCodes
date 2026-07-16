a151a786e173bf9579d6d0fd1c9b536b45c2b44d
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-07-16 23:41:27 +0530

    Added Projects pull in

--- server/controllers/landingController.js
+++ server/controllers/landingController.js
@@ -1,10 +1,13 @@
 const services = require("../models/services");
+const projects = require("../models/projects");
 
 const landingController = async (req, res) => {
   try {
     const allService = await services.find();
+    const allProject = await projects.find();
     console.log(allService);
-    res.status(200).json({ allService });
+    console.log(allProject);
+    res.status(200).json({ allService, allProject });
   } catch (err) {
     res.status(500).json({ error: err });
   }
